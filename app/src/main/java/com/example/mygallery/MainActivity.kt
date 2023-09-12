6d99e213b0bf1896418cea43ece9058a2f28b2dc
leejinseok <sonaky47@gmail.com>
2023-09-12 09:45:15 +0900

    권한요청

--- app/src/main/java/com/example/mygallery/MainActivity.kt
+++ app/src/main/java/com/example/mygallery/MainActivity.kt
@@ -1,28 +1,73 @@
 package com.example.mygallery
 
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.mygallery.ui.theme.MyGalleryTheme
+import androidx.core.content.ContextCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyGalleryTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+            var granted = remember {
+                mutableStateOf(false)
+            }
+
+            val launcher =
+                rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+                    granted.value = isGranted
+                }
+
+            val isAndroidVersionBeforeTiramisu =
+                android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.TIRAMISU
+
+            if (isAndroidVersionBeforeTiramisu) {
+                if (ContextCompat.checkSelfPermission(
+                        this,
+                        android.Manifest.permission.READ_EXTERNAL_STORAGE
+                    ) == PackageManager.PERMISSION_GRANTED
                 ) {
-                    Greeting("Android")
+                    granted.value = true
+                }
+            } else {
+                if (ContextCompat.checkSelfPermission(
+                        this,
+                        android.Manifest.permission.READ_MEDIA_IMAGES
+                    ) == PackageManager.PERMISSION_GRANTED
+                ) {
+                    granted.value = true
+                }
+            }
+
+            if (granted.value) {
+
+
+            } else {
+                if (isAndroidVersionBeforeTiramisu) {
+                    PermissionRequestScreen {
+                        launcher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                    }
+                } else {
+                    PermissionRequestScreen {
+                        launcher.launch(android.Manifest.permission.READ_MEDIA_IMAGES)
+                    }
                 }
             }
         }
@@ -30,17 +75,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyGalleryTheme {
-        Greeting("Android")
+fun PermissionRequestScreen(onClickPermissionRequestButton: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "권한이 허용되지 않았습니다")
+        Button(onClick = onClickPermissionRequestButton) {
+            Text(text = "권한 요청")
+        }
     }
 }
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun HomeScreen(photoUris: List<Uri>) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        HorizontalPager(pageCount = 3, modifier = Modifier.weight(1f)) { pageIndex ->
+            Card {
+
+            }
+        }
+    }
+}
+
